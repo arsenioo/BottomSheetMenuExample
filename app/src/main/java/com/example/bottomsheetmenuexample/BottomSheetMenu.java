6224dbbb51208fa9289f10b7cd6cb5f1f4f31ffd
Alexey <alexey@softick.com>
2019-05-16 17:55:09 +0300

    First steps

--- app/src/main/java/com/example/bottomsheetmenuexample/BottomSheetMenu.java
+++ app/src/main/java/com/example/bottomsheetmenuexample/BottomSheetMenu.java
@@ -1,28 +1,39 @@
 package com.example.bottomsheetmenuexample;
 
-import android.annotation.TargetApi;
 import android.content.Context;
-import android.os.Build;
+import android.os.Handler;
 import android.util.AttributeSet;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
 
 import androidx.annotation.Nullable;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
 
 /**
  * Created by apc on 2019-05-15 for BottomSheetMenuExample
  */
 
-public class BottomSheetMenu extends LinearLayout {
+public class BottomSheetMenu extends CoordinatorLayout {
     private View _topView;
     private View _bottomView;
+    private int topViewHeight = 0;
+    private BottomSheetBehavior bottomSheetBehavior;
+    private View menuControlButton;
+    private View exitButton;
+    private int currentState;
+    Handler handler;
+    private boolean _menuButtonEnabled;
 
     final LayoutParams matchParentParams =
         new LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
 
     public BottomSheetMenu(Context context) {
         super(context);
+        LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        layoutInflater.inflate(R.layout.bottom_menu, BottomSheetMenu.this, true);
     }
 
     public BottomSheetMenu(Context context, @Nullable AttributeSet attrs) {
@@ -31,11 +42,6 @@ public class BottomSheetMenu extends LinearLayout {
 
     public BottomSheetMenu(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-    }
-
-    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
-    public BottomSheetMenu(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
-        super(context, attrs, defStyleAttr, defStyleRes);
     }
 
     @Override
@@ -49,6 +55,9 @@ public class BottomSheetMenu extends LinearLayout {
         if (_bottomView != null) this.removeView(_bottomView);
         _bottomView = view;
         if (view != null) this.addView(view, -1, matchParentParams);
+        bottomSheetBehavior = BottomSheetBehavior.from(findViewById(R.id.bottomSheetLayout));
+        bottomSheetBehavior.setPeekHeight(topViewHeight);
+        currentState = BottomSheetBehavior.STATE_EXPANDED;
     }
 
     public void setTopView(View view)
@@ -56,6 +65,8 @@ public class BottomSheetMenu extends LinearLayout {
         if (_topView != null) this.removeView(_topView);
         _topView = view;
         if (view != null) this.addView(view, 0, matchParentParams);
+        _topView.measure(0,0);
+        topViewHeight = _topView.getMeasuredHeight();
+        if(bottomSheetBehavior != null) bottomSheetBehavior.setPeekHeight(topViewHeight);
     }
-
 }
--- app/src/main/java/com/example/bottomsheetmenuexample/MainActivity.java
+++ app/src/main/java/com/example/bottomsheetmenuexample/MainActivity.java
@@ -1,14 +1,60 @@
 package com.example.bottomsheetmenuexample;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.asynclayoutinflater.view.AsyncLayoutInflater;
 
 public class MainActivity extends AppCompatActivity {
+
+    ViewGroup rootLayout;
+    AsyncLayoutInflater mLayoutInflater;
+    BottomSheetMenu mMenu;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
         setContentView(R.layout.activity_main);
+        rootLayout = findViewById(android.R.id.content);
+        mLayoutInflater = new AsyncLayoutInflater(this);
+        createMenu();
     }
+
+    private void createMenu()
+    {
+        mMenu = new BottomSheetMenu(this);
+        rootLayout.addView(mMenu);
+        final AsyncLayoutInflater.OnInflateFinishedListener topViewCallback = new AsyncLayoutInflater.OnInflateFinishedListener()
+        {
+            @Override
+            public void onInflateFinished(View view, int resid, ViewGroup parent)
+            {
+                mMenu.setTopView(view);
+            }
+        };
+        final AsyncLayoutInflater.OnInflateFinishedListener bottomViewCallback = new AsyncLayoutInflater.OnInflateFinishedListener()
+        {
+            @Override
+            public void onInflateFinished(View view, int resid, ViewGroup parent)
+            {
+                mMenu.setBottomView(view);
+            }
+        };
+        mLayoutInflater.inflate(R.layout.menu_top_part, null, topViewCallback);
+        mLayoutInflater.inflate(R.layout.menu_bottom_part, null, bottomViewCallback);
+    }
+
+    public void onFirstButtonClick(View v){}
+    public void onSecondButtonClick(View v){}
+    public void onThirdButtonClick(View v){}
+    public void onFourthButtonClick(View v){}
+    public void onFifthButtonClick(View v){}
+    public void onSixthButtonClick(View v){}
+    public void onSeventhButtonClick(View v){}
+
 }
